b42fe0ce61945411ed31559bab247aec983157f2
Liza <obs4545@yandex.ru>
2023-02-15 01:09:05 +0300

    changed test buy product

---
+++ pages_and_components/components/login_registration_component.py
@@ -0,0 +1,45 @@
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from base.base_class import Base
+
+
+class Login_registration_component(Base):
+    def __init__(self, driver):
+        super().__init__(driver)
+        self.driver = driver
+
+        self.login_with_password = (By.XPATH, "//*[@class='form-login-sms']//*[@data-popup='email']")
+        self.email_input = (By.XPATH, "//*[@name='login_email']")
+        self.password_input = (By.XPATH, "//*[@name='login_password']")
+        self.login_button = (By.XPATH, "//*[@value='Войти']")
+        self.wait_for_loading
+
+
+    # Методы для авторизации
+    def click_login_with_password(self):
+        WebDriverWait(self.driver, 30).until(EC.visibility_of_element_located(self.login_with_password))
+        _login = self.driver.find_element(*self.login_with_password)
+        _login.click()
+        print("Click login with password")
+
+    def input_email(self, login_user):
+        WebDriverWait(self.driver, 30).until(EC.visibility_of_element_located(self.email_input))
+        _username = self.driver.find_element(*self.email_input)
+        _username.send_keys(login_user)
+        print("Input email")
+
+    def input_password(self, password_user):
+        WebDriverWait(self.driver, 30).until(EC.visibility_of_element_located(self.password_input))
+        _password = self.driver.find_element(*self.password_input)
+        _password.send_keys(password_user)
+        print("Input password")
+
+    def click_login_button(self):
+        WebDriverWait(self.driver, 30).until(EC.visibility_of_element_located(self.login_button))
+        _login_btn = self.driver.find_element(*self.login_button)
+        _login_btn.click()
+        print("Click login button")
+
+    def wait_for_loading(self):
+        WebDriverWait(self.driver, 30).until(EC.visibility_of_element_located(self.login_with_password))
